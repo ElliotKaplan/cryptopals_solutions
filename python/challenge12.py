863e4c99aa0b906540bcecbd4dbeccb10fd737cb
E. J. Kaplan <elliot.kaplan@gmail.com>
2018-05-17 08:54:20 -0400

    moved get_blocksize to utilities

--- python/challenge12.py
+++ python/challenge12.py
@@ -1,7 +1,7 @@
 from base64 import b64decode
 from string import printable
 
-from utilities import ecb_encrypt, randomkey
+from utilities import ecb_encrypt, randomkey, get_blocksize
 from challenge11 import tester
 printable = printable.encode()
 
@@ -11,17 +11,6 @@ unknown_string = b64decode(b'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24
 
 def encryptor(plaintext):
     return ecb_encrypt(plaintext+unknown_string, key)
-
-def get_blocksize(encryption):
-    # calculate the block size
-    testtext = b''
-    len1 = len(encryptor(testtext))
-    len2 = len1
-    while len2 == len1:
-        testtext += b'a'
-        len2 = len(encryptor(testtext))
-    blocksize = len2 - len1
-    return blocksize, len1 // blocksize
 
 def crack(encryption, blocksize, nblocks):
     plaintext = b'' # this is updated every time a new block is decrypted
