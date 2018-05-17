99c014c351174fe854cc574983de11766a25b6f0
E. J. Kaplan <elliot.kaplan@gmail.com>
2018-05-17 08:20:04 -0400

    moved crackign to main loop

--- python/challenge12.py
+++ python/challenge12.py
@@ -12,32 +12,39 @@ unknown_string = b64decode(b'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24
 def encryptor(plaintext):
     return ecb_encrypt(plaintext+unknown_string, key)
 
-# calculate the block size
-testtext = b''
-len1 = len(encryptor(testtext))
-len2 = len1
-while len2 == len1:
-    testtext += b'a'
-    len2 = len(encryptor(testtext))
-blocksize = len2 - len1
+def get_blocksize(encryption):
+    # calculate the block size
+    testtext = b''
+    len1 = len(encryptor(testtext))
+    len2 = len1
+    while len2 == len1:
+        testtext += b'a'
+        len2 = len(encryptor(testtext))
+    blocksize = len2 - len1
+    return blocksize, len1 // blocksize
+
+def crack(encryption, blocksize, nblocks):
+    plaintext = b'' # this is updated every time a new block is decrypted
+    for i in range(nblocks):
+        thisblock = b''
+        for j in range(blocksize-1, -1, -1):
+            pref = b'a'*j
+            unknown = encryptor(pref)[i*blocksize:(i+1)*blocksize]
+            for c in printable:
+                test_ecb = encryptor(pref+plaintext+thisblock+chr(c).encode())
+                if test_ecb[i*blocksize:(i+1)*blocksize] == unknown:
+                    thisblock += chr(c).encode()
+                    break
+        plaintext += thisblock
+    return plaintext
+            
+
 # confirm that this is an ecb encryption scheme
-assert tester(encryptor, blocksize) == 'ecb_encrypt'
+if __name__=='__main__':
 
-nblocks = len1 // blocksize
-plaintext = b'' # this is updated every time a new block is decrypted
+    blocksize, nblocks = get_blocksize(encryptor)
+    assert tester(encryptor, blocksize) == 'ecb_encrypt'
 
-for i in range(nblocks):
-    known = b'' # this is the known characters in this block 
-    for j in range(blocksize-1, -1, -1):
-        pref = b'a'*j
-        unknown = encryptor(pref)[i*blocksize:(i+1)*blocksize]
-        for c in printable:
-            test_ecb = encryptor(pref+plaintext+known+chr(c).encode())
-            if test_ecb[i*blocksize:(i+1)*blocksize] == unknown:
-                known += chr(c).encode()
-                break
-    plaintext += known
+    print(crack(encryptor, blocksize, nblocks))
 
-print(plaintext)
 
-    
